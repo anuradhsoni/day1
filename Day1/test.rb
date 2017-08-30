04765c52c10bbab7df9e38f80c95cea253a174df
anuradhasoni <asoni@grepruby.com>
2017-08-30 14:03:04 +0530

    first commit

---
+++ Day1/test.rb
@@ -0,0 +1,4 @@
+print <<EOF
+  This is the first way of creating
+  here document ie. mult
+EOF
---
+++ Day1/test2.rb
@@ -0,0 +1,8 @@
+    
+
+$i = 1
+$num = 100
+begin
+   puts("Inside the loop i = #$i" )
+   $i +=1
+end while $i < $num
